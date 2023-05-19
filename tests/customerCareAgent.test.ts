667e19f307cb570e72109fcd6affefaf970a6931
Dennis Ngeno <dennis.ngeno.andela@alation.com>
2023-05-19 16:52:36 +0300

    Add update and login

--- tests/customerCareAgent.test.ts
+++ tests/customerCareAgent.test.ts
@@ -1,5 +1,5 @@
 import request from 'supertest'
-import app from '../app'
+import app from '../src/app'
 import {PrismaClient} from "@prisma/client";
 
 const AUTH_ROUTE = '/users/customer_agent/create-user-account'
