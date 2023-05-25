32456f7acab24fd6411be4696ed26bc452950df8
Dennis Ngeno <dennis.ngeno.andela@alation.com>
2023-05-25 12:57:52 +0300

    - ensure authentication is working

---
+++ src/helpers/enums.ts
@@ -0,0 +1,5 @@
+export const enum UserType {
+    USER,
+    CCA,
+    RIDER
+}
--- src/routes/customerCareAgentAuthRoutes.ts
+++ src/routes/customerCareAgentAuthRoutes.ts
@@ -2,18 +2,19 @@ import express from "express";
 
 import {
     CCAVerification,
-    CustomerCareAgent,
+    CustomerCareAgent, getAllCCA,
     GetProfileCCA,
     LoginCCA,
     UpdateCCA
 } from '../controllers/CustomerCareAgentController';
+import {auth} from "../helpers/CreateToken";
 
 const router=express.Router();
 router.post("/create-user-account", CustomerCareAgent)
-// router.get("/create-user-account", CustomerCareAgent)
+router.get("/create-user-account", auth, getAllCCA)
 router.post("/login", LoginCCA)
-router.patch("/create-user-account/:id", UpdateCCA)
-router.get("/create-user-account/:id", GetProfileCCA)
-router.get("/verify/:id/:token", CCAVerification)
+router.patch("/create-user-account/:id", auth, UpdateCCA)
+router.get("/create-user-account/:id",auth, GetProfileCCA)
+router.get("/verify/:id/:token", auth, CCAVerification)
 
 export default router;
