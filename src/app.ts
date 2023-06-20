f8b077e476c9726f9d1c311d4ce2a70912e7bf0a
Dennis Ngeno <dennis.ngeno.andela@alation.com>
2023-06-20 12:02:12 +0300

    - add swagger

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import express, {Express, Request, Response} from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import swaggerUi from 'swagger-ui-express';
 import UserAuthRoutes from "./routes/userAuthRoutes";
 import CustomerCareAgentRoutes from "./routes/customerCareAgentAuthRoutes";
 import RiderAuthRoutes from "./routes/RiderAuthRoutes";
 import mpesaRoutes from "./routes/mpesaRoutes";
-
+import swaggerDocument from './swagger.json'
 import UserOTPRoutes from './routes/userOTPRoutes'
 
 const app: Express = express();
-
+var options = {
+    explorer: true
+  };
 app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
@@ -18,11 +21,12 @@ app.use(
 );
 app.use(cors());
 //routes
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 app.use("/api/users/auth", UserAuthRoutes);
 app.use("/api/riders/auth", RiderAuthRoutes);
 app.use("/api/points", mpesaRoutes);
 app.use("/api/users/auth/OTP", UserOTPRoutes);
-app.use("/api/users/customer_agent", CustomerCareAgentRoutes);
+app.use("/api/customer_agent", CustomerCareAgentRoutes);
 app.get("/api/", (req: Request, res: Response) => {
     res.send("test route...");
 });
